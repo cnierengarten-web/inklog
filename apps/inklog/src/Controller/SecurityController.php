7aa0597df1b1eb34af01aa442b6d03de35ac37cc
cnierengarten <claire.nierengarten@gmail.com>
2025-09-10 22:33:48 +0200

    fix(security): fix comment - micro refactor user

--- apps/inklog/src/Controller/SecurityController.php
+++ apps/inklog/src/Controller/SecurityController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -31,9 +30,7 @@ class SecurityController extends AbstractController
         ]);
     }
 
+    /** @codeCoverageIgnore */
     #[Route(path: '/logout', name: 'app_logout', methods: ['POST'])]
-    public function logout(): void
-    {
-        throw new LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
+    public function logout(): void { }
 }
