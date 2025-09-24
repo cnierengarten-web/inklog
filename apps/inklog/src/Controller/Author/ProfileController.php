c71072733199399f7ec9b398c151e6eb0fa4f0aa
cnierengarten <claire.nierengarten@gmail.com>
2025-09-24 04:04:01 +0200

    feat(front): Display last published articles by author, category and tags - add and adapt controller and template

--- apps/inklog/src/Controller/Author/ProfileController.php
+++ apps/inklog/src/Controller/Author/ProfileController.php
@@ -2,20 +2,40 @@
 
 namespace App\Controller\Author;
 
+use App\Entity\User;
+use App\Repository\Blog\ArticleRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('ROLE_USER')]
 #[Route('/author', name: 'author_')]
 final class ProfileController extends AbstractController
 {
     #[Route('', name: 'index', methods: ['GET'])]
-    public function index(): Response
-    {
+    #[IsGranted('ROLE_USER')]
+    public function index(
+        ArticleRepository $articleRepository,
+    ): Response {
+        $articles = $articleRepository->findLastPublishByAuthorSlug($this->getUser()->getSlug());
+
         return $this->render('author/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'articles' => $articles,
+        ]);
+    }
+
+    #[Route('/{slug}', name: 'by_slug', methods: ['GET'])]
+    public function authorBySlug(
+        ArticleRepository $articleRepository,
+        #[MapEntity(mapping: ['slug' => 'slug'])]
+        User $author,
+    ): Response {
+        $articles = $articleRepository->findLastPublishByAuthorSlug($author->getSlug());
+
+        return $this->render('author/by-slug.html.twig', [
+            'articles' => $articles,
+            'author' => $author,
         ]);
     }
 }
--- apps/inklog/src/Repository/Blog/ArticleRepository.php
+++ apps/inklog/src/Repository/Blog/ArticleRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository\Blog;
 
 use App\Entity\Blog\Article;
+use DateTimeImmutable;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,5 +15,81 @@ class ArticleRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Article::class);
+    }
+
+    public function findLastPublishedByTag(string $tagSlug, int $limit = 6): array
+    {
+        return $this->findLastPublishedBy($limit, null, null, $tagSlug);
+    }
+
+    public function findLastPublishByCategory(string $categorySlug, int $limit = 6): array
+    {
+        return $this->findLastPublishedBy($limit, null, $categorySlug);
+    }
+
+    public function findLastPublishByAuthorSlug(string $authorSlug, int $limit = 6): array
+    {
+        return $this->findLastPublishedBy($limit, $authorSlug);
+    }
+
+    public function findLastPublished(int $limit = 6): array
+    {
+        return $this->findLastPublishedBy($limit);
+    }
+
+    public function findLastPublishedBy(
+        int $limit = 10,
+        ?string $authorSlug = null,
+        ?string $categorySlug = null,
+        ?string $tagSlug = null,
+    ): array {
+        $idsQuery = $this->createQueryBuilder('a')
+            ->select('a.id')
+            ->where('a.publishedAt IS NOT NULL')
+            ->andWhere('a.publishedAt <= :now')
+            ->setParameter('now', new DateTimeImmutable());
+
+        if (isset($authorSlug)) {
+            $idsQuery->innerJoin('a.author', 'u')
+                ->andWhere('u.slug = :userslug')
+                ->setParameter('userslug', $authorSlug);
+        }
+
+        if (isset($categorySlug)) {
+            $idsQuery->innerJoin('a.categories', 'c')
+                ->andWhere('c.slug = :categoryslug')
+                ->setParameter('categoryslug', $categorySlug);
+        }
+
+        if (isset($tagSlug)) {
+            $idsQuery->innerJoin('a.tags', 't')
+                ->andWhere('t.slug = :tagslug')
+                ->setParameter('tagslug', $tagSlug);
+        }
+
+        $ids = $idsQuery
+            ->orderBy('a.publishedAt', 'DESC')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getScalarResult();
+        $ids = array_column($ids, 'id');
+
+        if (empty($ids)) {
+            return [];
+        }
+
+        return $this->createQueryBuilder('a')
+            ->select('a')
+            ->leftJoin('a.author', 'u')
+            ->addSelect('u')
+            ->leftJoin('a.categories', 'c')
+            ->addSelect('c')
+            ->leftJoin('a.tags', 't')
+            ->addSelect('t')
+            ->where('a.id IN (:ids)')
+            ->setParameter('ids', $ids)
+            ->orderBy('a.publishedAt', 'DESC')
+            ->getQuery()
+            ->getResult();
     }
 }
